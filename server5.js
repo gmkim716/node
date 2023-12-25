af1d64504aa291c9355afc92c07e50caba6a5aef
Jayden, Gyeongmin Kim <starlight3775@gmail.com>
2023-12-25 16:04:15 +0900

    p154 restServer.js 해야함

--- server5.js
+++ server5.js
@@ -3,7 +3,6 @@ const fs = require('fs');
 const url = require('url'); 
 const qs = require('querystring'); 
 
-// 쿠키의 키, 값 매핑 
 const parseCookies = (cookie = '') => 
 	cookie
 		.split(';')
@@ -14,7 +13,8 @@ const parseCookies = (cookie = '') =>
 			return acc;
 		}, {})
 
-// req에 따른 역할 수행
+const session = {};
+
 http.createServer((req, res) => {
 	const cookies = parseCookies(req.headers.cookie);
 	console.log(req.url, cookies); 
@@ -25,13 +25,20 @@ http.createServer((req, res) => {
 		const expires = new Date(); 
 
 		expires.setMinutes(expires.getMinutes() + 5); 
-		res.writeHead(302, { Location: '/', 'Set-Cookie': `name=${encodeURIComponent(name)}; Expires=${expires.toGMTString()}; HttpOnly; Path=/`,});
+		const randomInt =+ new Date();
+		session[randomInt] = { name, expires };
+
+		res.writeHead(302, { 
+			Location: '/', 
+			'Set-Cookie': `session=${randomInt}; Expires=${expires.toGMTString()}; HttpOnly; Path=/`,});
 		res.end(); 
 	} 
 	
-	else if (cookies.name) {
+	// 쿠키에 이름을 담아서 보내는 대신, randomInt라는 임의의 숫자를 보냄
+	// cookie.session이 있고, 만료 기한을 넘기지 않았다면, session 변수에서 사용자 정보를 가져와 사용 
+	else if (cookies.session && session[cookies.session].expires > new Date()) {
 		res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-		res.end(`${cookies.name}님 안녕하세요`);
+		res.end(`${session[cookies.session].name}님 안녕하세요`);
 	} 
 	
 	else {
@@ -43,7 +50,7 @@ http.createServer((req, res) => {
 		})
 	}
 })
-	.listen(8083, () => {
-		console.log('8083번 포트에서 서버 대기 중')
+	.listen(8084, () => {
+		console.log('8084번 포트에서 서버 대기 중')
 	});
 
