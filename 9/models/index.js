c7094a69d14cd16ca972d3c41f966a4b1cfba3ad
Jayden, Gyeongmin Kim <starlight3775@gmail.com>
2024-01-21 02:38:29 +0900

    init ch9

---
+++ 9/models/index.js
@@ -0,0 +1,30 @@
+const Sequelize = require("sequelize");
+const env = process.env.NODE_ENV || "development";
+const config = require("../config/config")[env];
+const User = require("./user");
+const Post = require("./post");
+const Hashtag = require("./hashtag");
+
+const db = {};
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
+
+db.sequelize = sequelize;
+db.User = User;
+db.Post = Post;
+db.Hashtag = Hashtag;
+
+// 각 모델을 시퀄라이즈 객체에 연결
+User.init(sequelize);
+Post.init(sequelize);
+Hashtag.init(sequelize);
+
+User.associate(db);
+Post.associate(db);
+Hashtag.associate(db);
+
+module.exports = db;
