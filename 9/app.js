0c121e71ae516fdf912ec3b01dd583458bd21aff
Jayden, Gyeongmin Kim <starlight3775@gmail.com>
2024-01-21 14:13:27 +0900

    p401. 프로젝트 구조 갖추기

---
+++ 9/app.js
@@ -0,0 +1,54 @@
+const express = require('express'); // express 모듈을 가져온다.
+const cookieParser = require('cookie-parser'); // 쿠키를 사용하기 위한 미들웨어
+const morgan = require('morgan'); // 로그 
+const path = require('path');
+const session = require('express-session'); 
+const nunjucks = require('nunjucks'); // 템플릿 엔진
+const dotenv = require('dotenv'); // 환경 변수
+
+dotenv.config(); // .env 파일을 읽어서 process.env로 만든다.
+const pageRouter = require('./routes/page');
+
+const app = express(); // express 객체 생성
+app.set('port', process.env.PORT || 8001);
+app.set('view engine', 'html'); // 템플릿 엔진 설정
+nunjucks.configure('views', { // 템플릿 파일들이 위치한 폴더 설정
+  express: app,
+  watch: true,
+});
+
+app.use(morgan('dev')); // 로그
+app.use(express.static(path.join(__dirname, 'public'))); // 정적 파일 제공
+app.use(express.json()); // body-parser
+app.use(express.urlencoded({ extended: false })); // body-parser
+app.use(cookieParser(process.env.COOKIE_SECRET)); // 쿠키
+app.use(session({ // 세션
+  resave: false, // 요청이 올 때 세션에 수정사항이 생기지 않더라도 세션을 다시 저장할지 설정
+  saveUninitialized: false, // 세션에 저장할 내역이 없더라도 세션을 저장할지 설정
+  secret: process.env.COOKIE_SECRET, 
+  cookie: {
+    httpOnly: true, // 자바스크립트로 쿠키에 접근하지 못하게 한다.
+    secure: false, // https를 쓸 때 true
+  },
+}));
+
+app.use('/', pageRouter);
+
+// 404 처리 미들웨어
+app.use((req, res, next) => { 
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error); // 에러를 넘겨준다.
+});
+
+// 에러 처리 미들웨어
+app.use((err, req, res, next) => { 
+  res.locals.message = err.message; // 템플릿 엔진 변수
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {}; // 템플릿 엔진 변수
+  res.status(err.status || 500); // 상태 코드
+  res.render('error'); // 에러 페이지
+});
+
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기 중');
+});
---
+++ 9/routes/page.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.use((req, res, next) => {  // 모든 라우터에 공통으로 적용되는 미들웨어  
+  res.locals.user = null;
+  res.locals.followerCount = 0;
+  res.locals.followingCount = 0;
+  res.locals.followerIdList = [];
+  next();
+});
+
+router.get('/profile', (req, res) => {
+  res.render('profile', { title: '내 정보 - NodeBird' });
+});
+
+router.get('/join', (req, res) => {
+  res.render('join', { title: '회원가입 - NodeBird' });
+});
+
+router.get('/', (req, res, next) => {
+  const twits = [];
+  res.render('main', {
+    title: 'NodeBird',
+    twits,
+  });
+});
+
+module.exports = router;
