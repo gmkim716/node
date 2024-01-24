3027c3544babe3841f0f9d1e1e71fbc77e7d875b
Jayden, Gyeongmin Kim <starlight3775@gmail.com>
2024-01-25 01:19:42 +0900

    p431. before kakao id get

--- 9/routes/page.js
+++ 9/routes/page.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
+const express = require("express");
+const { isLoggedIn, isNotLoggedIn } = require("./middlewares");
 
 const router = express.Router();
 
-router.use((req, res, next) => {  // 모든 라우터에 공통으로 적용되는 미들웨어  
+router.use((req, res, next) => {
+  // 모든 라우터에 공통으로 적용되는 미들웨어
   res.locals.user = req.user; // res.locals 객체에 user 속성을 추가한다.
   res.locals.followerCount = 0;
   res.locals.followingCount = 0;
@@ -11,18 +12,18 @@ router.use((req, res, next) => {  // 모든 라우터에 공통으로 적용되�
   next();
 });
 
-router.get('/profile', isLoggedIn, (req, res) => {
-  res.render('profile', { title: '내 정보 - NodeBird' });
+router.get("/profile", isLoggedIn, (req, res) => {
+  res.render("profile", { title: "내 정보 - NodeBird" });
 });
 
-router.get('/join', isNotLoggedIn, (req, res) => {
-  res.render('join', { title: '회원가입 - NodeBird' });
+router.get("/join", isNotLoggedIn, (req, res) => {
+  res.render("join", { title: "회원가입 - NodeBird" });
 });
 
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
   const twits = [];
-  res.render('main', {
-    title: 'NodeBird',
+  res.render("main", {
+    title: "NodeBird",
     twits,
   });
 });
